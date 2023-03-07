fe4dd7351e2b675d6d9c79d70b94e66823db9c6a
Erik Anderson <108945295+ErikAndersonWebDev@users.noreply.github.com>
2023-03-06 21:18:07 -0800

    Exercise 7 completed

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,8 +9,8 @@ puts "Exercise 6"
 puts "----------"
 
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
-@store1.employees.create(first_name: "Abe", last_name: "Glabe", hourly_rate: 10)
-@store1.employees.create(first_name: "Bob", last_name: "Snob", hourly_rate: 20)
-@store2.employees.create(first_name: "Cassy", last_name: "Sassy", hourly_rate: 30)
-@store2.employees.create(first_name: "Danny", last_name: "Manny", hourly_rate: 40)
-@store2.employees.create(first_name: "Erie", last_name: "Deary", hourly_rate: 50)
+@store1.employees.create(first_name: "Abe", last_name: "Glabe", hourly_rate: 80)
+@store1.employees.create(first_name: "Bob", last_name: "Snob", hourly_rate: 100)
+@store2.employees.create(first_name: "Cassy", last_name: "Sassy", hourly_rate: 120)
+@store2.employees.create(first_name: "Danny", last_name: "Manny", hourly_rate: 140)
+@store2.employees.create(first_name: "Erie", last_name: "Deary", hourly_rate: 160)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,15 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+Employee.validates :first_name, presence: true
+Employee.validates :last_name, presence: true
+Employee.validates :hourly_rate, inclusion: {in: 40...200}
+Store.validates :name, length: {minimum: 3}
+Store.validates :annual_revenue, numericality: {only_integer: true, minimum: 0}
+
+print "Please enter a store name: "
+test = gets 
+store = Store.create(name: test)
+store.errors.full_messages.each do |msg| 
+  puts msg 
+end
